9dcfe3573fa7f07916055ca1ecd2981153b6a42b
Pedro Henrique Vermes <pedrohvermes@gmail.com>
2025-03-04 02:20:59 -0300

    Add project files.

---
+++ AtividadeAppMinhasCompras/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AtividadeAppMinhasCompras;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
